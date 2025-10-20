cdd67f891efe7db1ddd0afe01cf97b86021e4f8b
DanielOluwakemi <danieloluwakemi69@gmail.com>
2025-10-20 15:22:24 +0100

    first commit

---
+++ src/test/java/com/plutospace/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plutospace.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
